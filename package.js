bb1823744174eead57807c6d6074e17d5a14211f
David Glasser <glasser@davidglasser.net>
2014-06-20 15:09:35 -0700

    Wraps npm package pathwatcher
    Not useful

---
+++ package.js
@@ -0,0 +1,10 @@
+Package.describe({
+  name: "meteor-pathwatcher",
+  version: "1.0.0"
+});
+
+Npm.depends({"pathwatcher": "1.3.2"});
+
+Package.on_use(function (api) {
+  api.add_files('pw.js', 'server');
+});
